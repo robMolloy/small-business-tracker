5388d58286db508f850475645021ae1eb41cfeee
robMolloy <robmolloy@hotmail.co.uk>
2021-01-27 23:45:05 +0000

    import then use context trio

--- src/contexts/custom/AllContextProviders.js
+++ src/contexts/custom/AllContextProviders.js
@@ -1,24 +1,24 @@
 import React from "react";
-import { ContactContextProvider } from "./ContactContext";
-import { CustomerContextProvider } from "./CustomerContext";
-import { PrjCusLinkContextProvider } from "./PrjCusLinkContext";
-import { ProjectContextProvider } from "./ProjectContext";
-import { RecItemContextProvider } from "./RecItemContext";
-import { RecordContextProvider } from "./RecordContext";
+import ContactContext from "./ContactContext";
+import CustomerContext from "./CustomerContext";
+import PrjCusLinkContext from "./PrjCusLinkContext";
+import ProjectContext from "./ProjectContext";
+import RecItemContext from "./RecItemContext";
+import RecordContext from "./RecordContext";
 
 const AllContextProviders = ({ children }) => {
   return (
-    <ContactContextProvider>
-      <CustomerContextProvider>
-        <PrjCusLinkContextProvider>
-          <ProjectContextProvider>
-            <RecItemContextProvider>
-              <RecordContextProvider>{children}</RecordContextProvider>
-            </RecItemContextProvider>
-          </ProjectContextProvider>
-        </PrjCusLinkContextProvider>
-      </CustomerContextProvider>
-    </ContactContextProvider>
+    <ContactContext.Provider>
+      <CustomerContext.Provider>
+        <PrjCusLinkContext.Provider>
+          <ProjectContext.Provider>
+            <RecItemContext.Provider>
+              <RecordContext.Provider>{children}</RecordContext.Provider>
+            </RecItemContext.Provider>
+          </ProjectContext.Provider>
+        </PrjCusLinkContext.Provider>
+      </CustomerContext.Provider>
+    </ContactContext.Provider>
   );
 };
 
