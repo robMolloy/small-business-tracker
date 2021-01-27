b4d410ad3f0f604cea912aba4e688f8e7eb286ec
robMolloy <robmolloy@hotmail.co.uk>
2021-01-27 12:04:10 +0000

    no styloing of form

--- src/components/generic/forms/Form.js
+++ src/components/generic/forms/Form.js
@@ -2,18 +2,17 @@ import React from "react";
 import makeStyles from "@material-ui/styles/makeStyles";
 
 const Form = ({ children, ...props }) => {
-    const classes = makeStyles((theme) => ({
-        root: {
-            width: "100%",
-            marginTop: theme.spacing(1),
-        },
-    }))();
+  const classes = makeStyles((theme) => ({
+    root: {
+      //   width: "100%",
+    },
+  }))();
 
-    return (
-        <form noValidate {...props} className={classes.root}>
-            {children}
-        </form>
-    );
+  return (
+    <form noValidate {...props} className={classes.root}>
+      {children}
+    </form>
+  );
 };
 
 export default Form;
