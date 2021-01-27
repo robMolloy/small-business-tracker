68e1225d0a4bf44a716df3c711f07fe9bbb38cf2
robMolloy <robmolloy@hotmail.co.uk>
2021-01-27 23:35:02 +0000

    logic for storing different varTypes added

--- src/contexts/ContextProvider.js
+++ src/contexts/ContextProvider.js
@@ -1,28 +1,39 @@
 import React from "react";
-import Reducer from "../reducers/Reducer";
 import mightyStorage from "../classes/mightyStorage";
 import config from "../config";
 
+import ArrayReducer from "./ArrayReducer";
+import NumberReducer from "./NumberReducer";
+import ObjectReducer from "./ObjectReducer";
+
 const ContextProvider = (props) => {
-  let itemType, Context;
-  ({ itemType, Context } = props);
+  let itemType, Context, children, varType, afterSetState;
+  ({
+    itemType,
+    Context,
+    children,
+    varType,
+    afterSetState = mightyStorage.setItem,
+  } = props);
+
+  let storageType = { object: {}, array: [], number: 0 }[varType];
+  let Reducer = {
+    object: ObjectReducer,
+    array: ArrayReducer,
+    number: NumberReducer,
+  }[varType];
 
   const storageLocation = `${config.project.acronym}_${itemType}`;
 
-  const initStorage = () => mightyStorage.getItem(storageLocation, {});
-  // const setStorage = ;
-
+  const initStorage = () => mightyStorage.getItem(storageLocation, storageType);
   const [items, dispatch] = React.useReducer(Reducer, initStorage());
 
-  React.useEffect(() => mightyStorage.setItem(storageLocation, items), [
-    storageLocation,
-    items,
-  ]);
+  React.useEffect(() => {
+    afterSetState(storageLocation, items);
+  }, [storageLocation, afterSetState, items]);
 
   return (
-    <Context.Provider value={{ items, dispatch }}>
-      {props.children}
-    </Context.Provider>
+    <Context.Provider value={{ items, dispatch }}>{children}</Context.Provider>
   );
 };
 
