ee7595afc1434d1d57e2d1083b45fa61a2982914
robMolloy <robmolloy@hotmail.co.uk>
2021-01-29 01:49:46 +0000

    New Icon

---
+++ src/components/generic/icons/ContactIcon.js
@@ -0,0 +1,5 @@
+import React from "react";
+import MUIContactIcon from "@material-ui/icons/Contact";
+import Icon from "./Icon";
+
+export default (props) => <Icon Component={MUIContactIcon} {...props} />;
