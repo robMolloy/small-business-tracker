18bbf54d1fc7e506f76039a08d1e6aba78bf288c
robMolloy <robmolloy@hotmail.co.uk>
2021-02-01 02:38:18 +0000

    dual list

--- src/components/pages/Customers.js
+++ src/components/pages/Customers.js
@@ -3,24 +3,21 @@ import CustomerMultiform from "../custom/multiforms/CustomerMultiform";
 import Section from "../generic/sections/Section";
 import GridContainer from "../generic/grids/GridContainer";
 import ContactsOnCustomersList from "../custom/lists/dual-lists/ContactsOnCustomersList";
-// import CustomerContext from "../../contexts/custom/single-contexts/CustomerContext";
-// import ContactContext from "../../contexts/custom/single-contexts/ContactContext";
 
-import DualContactsOnCustomersContext from "../../contexts/custom/dual-contexts/DualContactsOnCustomersContext";
+import useContactsOnCustomersContext from "../../contexts/custom/dual-contexts/useContactsOnCustomersContext";
 
 const Customers = () => {
-  const { items } = DualContactsOnCustomersContext();
-  // const { items: customers } = CustomerContext.useContext();
-  // const { items: contacts } = ContactContext.useContext();
+  const { items } = useContactsOnCustomersContext();
   //
   return (
     <>
-      <CustomerMultiform />
+      <Section>
+        <CustomerMultiform />
+      </Section>
 
       <Section>
         <GridContainer>
           <ContactsOnCustomersList values={items} />
-          {/* <ContactsOnCustomersList /> */}
         </GridContainer>
       </Section>
     </>
