266b040550a7ca34001aa695fca97f83743a8d65
robMolloy <robmolloy@hotmail.co.uk>
2021-02-01 02:39:48 +0000

    context used elsewhere

--- src/components/custom/lists/dual-lists/ContactsOnCustomersList.js
+++ src/components/custom/lists/dual-lists/ContactsOnCustomersList.js
@@ -2,35 +2,15 @@ import React from "react";
 import DualItemList from "../../../generic/lists/DualItemList";
 import CustomerLineDisplayContents from "../../display-contents/CustomerLineDisplayContents";
 import ContactList from "../single-lists/ContactList";
-import DualContactsOnCustomersContext from "../../../../contexts/custom/dual-contexts/DualContactsOnCustomersContext";
-import CustomerContext from "../../../../contexts/custom/single-contexts/CustomerContext";
-import ContactContext from "../../../../contexts/custom/single-contexts/ContactContext";
-//
 
 const ContactsOnCustomersList = (props) => {
   let values;
   ({ values } = props);
 
-  // const { items } = DualContactsOnCustomersContext();
-
-  let { items: customers } = CustomerContext.useContext();
-  const { items: contacts } = ContactContext.useContext();
-  console.log("test", customers);
-
-  customers = JSON.parse(JSON.stringify(customers));
-
-  let rtn = {};
-
-  Object.entries(customers).forEach(([id, cus]) => (rtn[id] = { [id]: cus }));
-  // Object.entries(customers).forEach(([id, cus]) => (rtn[id][id] = cus));
-  Object.entries(contacts).forEach(
-    ([id, con]) => (rtn[con.con_cus_id][id] = con)
-  );
-
   return (
     <DualItemList
       {...{
-        values: rtn,
+        values: values,
         SummaryComponent: CustomerLineDisplayContents,
         DetailsComponent: ContactList,
       }}
