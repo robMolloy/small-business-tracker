2063637d023d48396968537b59c5048c721ba8ac
robMolloy <robmolloy@hotmail.co.uk>
2021-02-01 02:39:09 +0000

    keep naming convention

--- src/contexts/custom/dual-contexts/useContactsOnCustomersContext.js
+++ src/contexts/custom/dual-contexts/useContactsOnCustomersContext.js
@@ -1,17 +1,13 @@
 import ContactContext from "../single-contexts/ContactContext";
 import CustomerContext from "../single-contexts/CustomerContext";
 
-const DualContactsOnCustomersContext = () => {
+const useContactsOnCustomersContext = () => {
   let { items: contacts } = ContactContext.useContext();
   let { items: customers } = CustomerContext.useContext();
 
-  contacts = { ...contacts };
-  customers = { ...customers };
-
   let rtn = {};
 
-  Object.entries(customers).forEach(([id, cus]) => (rtn[id] = {}));
-  Object.entries(customers).forEach(([id, cus]) => (rtn[id][id] = cus));
+  Object.entries(customers).forEach(([id, cus]) => (rtn[id] = { [id]: cus }));
   Object.entries(contacts).forEach(
     ([id, con]) => (rtn[con.con_cus_id][id] = con)
   );
@@ -19,4 +15,4 @@ const DualContactsOnCustomersContext = () => {
   return { items: rtn };
 };
 
-export default DualContactsOnCustomersContext;
+export default useContactsOnCustomersContext;
