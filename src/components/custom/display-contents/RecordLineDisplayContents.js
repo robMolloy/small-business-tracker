58b8f081f5bf41c8bafd881ef1ec48b0a3881993
robMolloy <robmolloy@hotmail.co.uk>
2021-02-02 21:42:09 +0000

    formatting of record line

--- src/components/custom/display-contents/RecordLineDisplayContents.js
+++ src/components/custom/display-contents/RecordLineDisplayContents.js
@@ -1,8 +1,14 @@
+import normalizeCurrency from "../../../functions/normalizeCurrency";
+import getLocaleDateString from "../../../functions/getLocaleDateString";
+
 const RecordLineDisplayContents = (props) => {
   let values;
   ({ values } = props);
+  console.log(values);
 
-  return ` ${values.rec_timestamp_planned_start} ${values.rec_duration_qty} ${values.rec_duration_unit} ${values.rec_total}`;
+  return `${getLocaleDateString(values.rec_timestamp_planned_start)} ${
+    values.rec_duration_qty
+  } ${values.rec_duration_unit} ${normalizeCurrency(values.rec_total, true)}`;
 };
 
 export default RecordLineDisplayContents;
