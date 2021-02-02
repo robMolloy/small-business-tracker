fe8b2e316c6a0e72ae7087f6b1a447ed4bfa499f
robMolloy <robmolloy@hotmail.co.uk>
2021-02-02 09:57:29 +0000

    normalize currency - by default returns 2dp string

--- src/functions/normalizeCurrency.js
+++ src/functions/normalizeCurrency.js
@@ -1,8 +1,15 @@
-const normalizeCurrency = (qty, currency = "GBP") => {
-  return new Intl.NumberFormat("en-US", {
-    style: "currency",
-    currency,
-  }).format(qty);
+const normalizeCurrency = (qty, symbol = false, currency = "GBP") => {
+  if (!symbol) return Math.round(qty * 100) / 100;
+  else
+    return new Intl.NumberFormat("en-GB", {
+      style: "currency",
+      currency,
+    }).format(qty);
+  // .formatToParts(qty)
+  // .filter((rtn) => rtn.type !== "currency")
+  // .filter((rtn) => rtn.type !== "group")
+  // .map((rtn) => rtn.value)
+  // .join("");
 };
 
 export default normalizeCurrency;
