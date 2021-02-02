daddc002f36852d56496665f15c349ac93e6b0ff
robMolloy <robmolloy@hotmail.co.uk>
2021-02-02 09:53:45 +0000

    new icon added

---
+++ src/components/generic/icons/RefreshIcon.js
@@ -0,0 +1,6 @@
+import React from "react";
+import MUIRefreshIcon from "@material-ui/icons/Refresh";
+import Icon from "./Icon";
+
+const NewIcon = (props) => <Icon Component={MUIRefreshIcon} {...props} />;
+export default NewIcon;
